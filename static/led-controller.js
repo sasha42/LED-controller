d9dcede247ccfa388ed6aa11740a765d5e3ee5f7
sasha42 <sasha42@users.noreply.github.com>
2016-02-07 12:28:00 +0100

    Added angular slider and minor ng clean up

--- static/led-controller.js
+++ static/led-controller.js
@@ -1,8 +1,19 @@
 (function(angular) {
   'use strict';
-angular.module('led-controller', [])
-  .controller('LedController', function($http) {
+angular.module('led-controller', ['rzModule'])
+  .controller('LedController', function($http, $scope) {
+    $scope.create
     this.qty = 1;
+
+    $scope.slider = {
+        minValue: 10,
+        maxValue: 90,
+        options: {
+            floor: 0,
+            ceil: 100,
+            step: 1
+        }
+    };
 
     this.on = function on() {
 
