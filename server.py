886175543621cb604577e7cc461362352123a666
sasha42 <sasha42@users.noreply.github.com>
2016-02-12 20:16:32 +0100

    Added waiters handler for websockets on multiple devices

--- server.py
+++ server.py
@@ -2,33 +2,41 @@ import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
-from Adafruit_PWM_Servo_Driver import PWM
+#from Adafruit_PWM_Servo_Driver import PWM
 import time, sys, os, getopt, json
 
 # Initialise LED
-pwm = PWM(0x40)
-pwm.setPWMFreq(1000)
+#pwm = PWM(0x40)
+#pwm.setPWMFreq(1000)
 
 def parse_json(message):
     parameters = json.loads(message)
     for value in parameters:
         channel = int(value)
         brightness = (4095-int(parameters[value]))
-        pwm.setPWM(channel, brightness)
+        #pwm.setPWM(channel, brightness)
     return
 
 class WSHandler(tornado.websocket.WebSocketHandler):
+  waiters = set()
 
   def open(self):
     print ('user is connected.\n')
+    WSHandler.waiters.add(self)
 
   def on_message(self, message):
     print ('%s' %message)
     parse_json(message)
     self.write_message(message + ' OK')
+    for waiter in cls.waiters:
+        try:
+            waiter.write_message(chat)
+        except:
+            logging.error("Error sending message", exc_info=True)
 
   def on_close(self):
     print ('connection closed\n')
+    WSHandler.waiters.remove(self)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
