3f853eb64485deda95639490333218327216a6f1
sasha42 <sasha42@users.noreply.github.com>
2016-05-14 14:31:20 +0200

    Proper WebSocketHandler implementation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LED controller
+#LED controller
 
 Real time websockets LED controller with a PCA9685 board connected to a Raspberry Pi and some LED strips. Right now there is only one color-wheel with 3 channel outputs, however you can have up to 16 channels per board. All control is done via JSON, new channels and control options can be added with ease.
 
--- server.py
+++ server.py
@@ -18,21 +18,37 @@ def parse_json(message):
     return
 
 class WSHandler(tornado.websocket.WebSocketHandler):
+  waiters = set()
+  cache = []
+  cache_size = 200
 
   def open(self):
     print ('user is connected.\n')
+    WSHandler.waiters.add(self)
+  
+  @classmethod
+  def update_cache(self, message):
+        self.cache.append(message)
+        if len(self.cache) > self.cache_size:
+            self.cache = self.cache[-self.cache_size:]
 
   def on_message(self, message):
     print ('%s' %message)
     parse_json(message)
     self.write_message(message + ' OK')
+    WSHandler.update_cache(message)
+    for waiter in self.waiters:
+        try:
+            waiter.write_message(message)
+        except:
+            logging.error("Error sending message", exc_info=True)
 
   def on_close(self):
     print ('connection closed\n')
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("led.html")
+        self.render("led.html", messages=WSHandler.cache)
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
